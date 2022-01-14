e1a9c915346af63420b9d0a4ef2dd752167a6917
mhimf001 <flavian.machimbirike@vcontractor.co.za>
2022-01-14 21:34:18 +0200

    Product fix.

--- src/main/java/com/armsV2/armsApi/impl/ProductServiceImpl.java
+++ src/main/java/com/armsV2/armsApi/impl/ProductServiceImpl.java
@@ -1,18 +1,11 @@
 package com.armsV2.armsApi.impl;
 
 
-import com.armsV2.armsApi.dto.LoginResponseDto;
 import com.armsV2.armsApi.dto.ProductResponseDto;
-import com.armsV2.armsApi.exceptions.InvalidLoginException;
-import com.armsV2.armsApi.exceptions.UnexpectedErrorException;
 import com.armsV2.armsApi.models.Product;
-import com.armsV2.armsApi.repositories.EmployRepo;
 import com.armsV2.armsApi.repositories.ProductRepo;
-import com.armsV2.armsApi.services.AuthService;
 import com.armsV2.armsApi.services.ProductsService;
-import io.micrometer.core.instrument.util.StringUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
